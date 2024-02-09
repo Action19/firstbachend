7a39ccb61077d4cccff7c35bae4c7117cfd09afa
Action19 <uatskhk@gmail.com>
2024-02-09 20:45:37 +0500

    Update-09-02-2024

--- models/studentsSchema.js
+++ models/studentsSchema.js
@@ -3,6 +3,7 @@ const mongoose =  require('mongoose');
 const studentsSchema = new mongoose.Schema({
     firstname: {type: String, required: true},
     lastname: {type: String, required: true},
+    fathername: {type: String, required: true},
     grade: {type: String, required: true},
     image: {type: String},
     login: {type: String, unique: true},
