bf59bf3a3fc0595c6b73cfc07dd5712f8aa4b07d
Action19 <uatskhk@gmail.com>
2024-02-16 18:24:40 +0500

    Allow-Origin-delete

--- index.js
+++ index.js
@@ -8,11 +8,7 @@ const gradeRoutes = require('./routes/gradeRoutes');
 
 const app = express();
 app.use(cors());
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-    next();
-});
+
 require('./startup/prod')(app);
 
 app.use(express.urlencoded({extended: true}))
