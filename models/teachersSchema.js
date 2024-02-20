0bbb4e032c9716a3f662c9cdfe79a27e5ce5db55
Action19 <uatskhk@gmail.com>
2024-02-20 10:35:22 +0500

    teachersModelUpdate

--- models/teachersSchema.js
+++ models/teachersSchema.js
@@ -1,14 +1,14 @@
 const mongoose =  require('mongoose');
 
-const gradeSchema = new mongoose.Schema({
-    grades: {type: [ String ], required: true}
-});
-
 const teachersSchema = new mongoose.Schema({
     firstname: {type: String, required: true},
     lastname: {type: String, required: true},
     fathername: {type: String, required: true},
-    science: {type: [ gradeSchema ], required: true},
+    science: {
+        type: Map,
+        of: [String],
+        required: true
+      },
     image: {type: String},
     login: {type: String, unique: true},
     password: {type: String},
