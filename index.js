08ad39c49cadc4feb32a7122deff520ce23fa86b
Action19 <uatskhk@gmail.com>
2024-02-27 14:43:31 +0500

    Corsgauchun

--- index.js
+++ index.js
@@ -8,9 +8,13 @@ const teacherRoutes = require('./routes/teachersRoute');
 
 
 const app = express();
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+app.use(cors());
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*"); // Bu qatorda * o'rniga ma'lum bir domen yoki domenlar qatorini belgilashingiz mumkin
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+});
 
 require('./startup/prod')(app);
 
