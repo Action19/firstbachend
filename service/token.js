500fb08f926025fe68e9752565d842d39178a32b
Action19 <uatskhk@gmail.com>
2024-03-24 06:40:56 +0500

    tokenadd

--- service/token.js
+++ service/token.js
@@ -1,5 +1,7 @@
-import  Jwt  from 'jsonwebtoken'
-export const generateJWTToken = (userId, role) =>{
+const   Jwt = require('jsonwebtoken')
+
+module.exports =  function generateJWTToken(userId, role){
   const accessToken = Jwt.sign({userId, role}, 'simul@te.uz-0.0.1', {expiresIn: '30d'})
   return accessToken
 }
+
